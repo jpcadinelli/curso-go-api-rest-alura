56d9a8853b6b4043f3ff879c5d27e7e4e3989ad9
jpcadinelli <jpcadinelli@gmail.com>
2024-03-25 15:42:32 -0300

    aula 3 finalizada - requisições no banco estão funcionais

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/jpcadinelli/curso-go-api-rest-alura/database"
 	"github.com/jpcadinelli/curso-go-api-rest-alura/models"
 	"github.com/jpcadinelli/curso-go-api-rest-alura/routes"
 )
@@ -12,6 +13,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando servidor REST com Go.")
 	routes.HandleRequest()
 }
